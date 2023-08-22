d0f5b0d850b70f6df458b23f267749d39e4da079
kushitha-p <137861362+kushitha-p@users.noreply.github.com>
2023-08-22 16:12:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = "us-west-1"  
+    region = "ap-southeast-2"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-0f8e81a3da6e2510a" # us-west-1
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "kushitha01184"
+  acl    = "private"
   tags = {
-      Name = "TF-Instance"
+      Name = "My s3 bucket"
   }
 }
